479ac649f89c9228616ff8bfb2b06061f1f5ddeb
OhWhale515 <sterlinggreen515@gmail.com>
2022-12-30 14:12:17 -0500

    added needed sections for app routes

--- WebDevelopment/HTML/HTML-Forms/main.py
+++ WebDevelopment/HTML/HTML-Forms/main.py
@@ -1,15 +1,40 @@
 from flask import Flask, render_template, request
+import requests
+
+posts = requests.get("https://api.npoint.io/43644ec4f0013682fc0d").json()
 app = Flask(__name__)
 
-app.route('/')
-def home():
-    return render_template("index.html")
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
 
-@app.route('/login', methods=["POST"])
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+  
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/form-entry", methods=["POST"])
 def receive_data():
-    name  = request.form['username']
-    password = request.form['password']
-    return f"<h1>Name: {name}, Password: {password}</h1>"
+    data = request.form
+    print(data["name"])
+    print(data["email"])
+    print(data["phone"])
+    print(data["message"])
+    return "<h1>Successfully sent your message</h1>"
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=True)
